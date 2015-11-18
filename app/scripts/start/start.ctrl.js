fef65d39392b285cd3e6aa6a53c7168cbca2dd27
Aneta <necia425@gmail.com>
2015-11-18 18:58:12 +0100

    .

--- app/scripts/start/start.ctrl.js
+++ app/scripts/start/start.ctrl.js
@@ -10,5 +10,24 @@ angular.module('Rolnik')
   .controller('StartController', function($scope) {
 
     // do something with $scope
+    var vm = this;
+
+    this.auth = auth;
+
+    function auth() {
+    	console.log("a");
+
+
+		var tk = document.createElement('script');
+	    tk.src = 'https://apis.google.com/js/client.js';
+	    tk.type = 'text/javascript';
+	    tk.async = 'true';
+	    tk.onload = tk.onreadystatechange = function() {
+	      console.log("g");
+	    };
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(tk, s);
+
+	}
 
   });
